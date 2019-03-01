5f29adfe59beed8d0fde5a2954daec9f1ecc64be
Janniku9 <gartmann.jannik@gmail.com>
2019-03-01 09:29:16 +0100

    added reward and succes rate plot

--- MountainCar/qtable.py
+++ MountainCar/qtable.py
@@ -1,25 +1,29 @@
 import numpy as np 
 from numpy import random
 import gym
+import matplotlib.pyplot as plt
 
 # make new environment
 env = gym.make("MountainCar-v0")
 
 # number of episodes
-episodes = 10000
+episodes = 100000
 
 # learning rate a and exploration/exploitation rate e and discount rate d
 a = 0.2
 e = 1.0
 d = 0.9
 count = 0
+rewards = []
+success_rate = []
+success = 0.0
 
 # get space low and high
 low = env.observation_space.low
 high = env.observation_space.high
 
 # because space is not discrete i make it discrete with s points
-s = 100
+s = 200
 position_range = np.linspace(low[0], high[0], s)
 velocity_range = np.linspace(low[1], high[1], int(s/5))
 
@@ -35,15 +39,19 @@ for eps in range(episodes):
     old_position = np.searchsorted(position_range, 0)
     old_velocity = np.searchsorted(velocity_range, 0)
 
-    # init done and loop iterator
+    if eps % 1000 is 0:
+        print (eps)
+
+    # init done and reward
     done = False
+    total_reward = 0
 
     # update exploration/exploitation rate
     e = e - 1.0/episodes    
 
     # run episode
     while (not done):
-        # if eps >= episodes - 100 or eps < 5:
+        # if eps >= episodes - 100:
             # env.render()
 
         # calculate next step
@@ -61,11 +69,12 @@ for eps in range(episodes):
         reward = step[1]
         done = step[2]
 
+        total_reward += reward
+
         # check if car reached top
         if done and 0.47 <= position_range[position]:
             QTable[position, velocity, next_action] = reward
-            if eps >= episodes - 1000:
-                count = count + 1
+            success = success + 1.0
         
         # update QTable
         else:
@@ -74,5 +83,12 @@ for eps in range(episodes):
 
         old_position = position
         old_velocity = velocity
+    rewards.append(total_reward)
+    success_rate.append(success/(eps+1))
 
+plt.plot(success_rate)
+plt.show()
+
+plt.plot(rewards)
+plt.show()
 print("success: ", count)
